ce325eeea33593638dfc378a8ceb7623b81b7409
mthq2004 <maithanhhaiquan2004@gmail.com>
2025-06-24 21:20:48 +0700

    push

--- src/routes/RouterApp.jsx
+++ src/routes/RouterApp.jsx
@@ -1,32 +1,21 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Contact from "../pages/Contact";
 import Projects from "../pages/Projects";
 
-const router = createBrowserRouter([
-    {
-        path:"/my-porfolio/",
-        element: <App/>,
-        children:[
-            {
-                index: true,
-                element: <Home/>
-            },
-            {
-                path:"projects",
-                element: <Projects/>
-            },
-            {
-                path:"about",
-                element: <About/>
-            },
-            {
-                path:"contact",
-                element: <Contact/>
-            }
-        ]
-    }
-])
-export default router
+const router = createHashRouter([
+  {
+    path: "/", // 👉 không cần "my-porfolio" vì HashRouter xử lý khác
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "projects", element: <Projects /> },
+      { path: "about", element: <About /> },
+      { path: "contact", element: <Contact /> }
+    ]
+  }
+]);
+
+export default router;
