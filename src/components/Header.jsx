76350b2060154b443b290dd3fa729bbcdaf64c24
mthq2004 <maithanhhaiquan2004@gmail.com>
2025-06-24 21:48:13 +0700

    push

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <Container>
         <Navbar.Brand as={NavLink} to="/" className='d-flex justify-content-center align-items-center'>
           <img
-            src="/public/img/logo.png"
+            src="/public/logo.png"
             alt="Logo"
             style={{ maxHeight: '50px' }}
           />
